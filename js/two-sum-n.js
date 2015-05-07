e0b60615afd52e16ad058a0e18ee6d0cbc7611a6
dengyaolong <dengyaolong@yeah.net>
2015-05-08 00:29:11 +0800

    修改代码

--- js/two-sum-n.js
+++ js/two-sum-n.js
@@ -1,14 +1,9 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
 var twoSum = function(nums, target) {
   var index = [];
   for (var i = 0; i < nums.length; ++i) {
       var j = index[target-nums[i]];
       if (typeof j != "undefined") {
-          return i  < j ? [i + 1, j + 1] : [j + 1, i + 1];
+          return [j + 1, i + 1];
       }
       index[nums[i]] = i;
   }
