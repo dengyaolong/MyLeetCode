5f4ad7e9ceaec520980041b4365cd74ea71f4633
dengyaolong <dengyaolong@yeah.net>
2015-05-08 16:43:20 +0800

    修改指针迭代

--- js/2-add-two-numbers-list.js
+++ js/2-add-two-numbers-list.js
@@ -1,11 +1,13 @@
 var addTwoNumbers = function(l1, l2) {
-  var t = l1.val + l2.val;
-  var ans = cur = new ListNode(t%10);
-  while (!!(l1 = l1.next) | !! (l2 = l2.next) | !!(f = t > 9 ? 1 : 0)) {
+  var ans = cur = new ListNode(0), f = 0;
+  while (l1 || l2 || f) {
     if (!l1) { l1 = new ListNode(0); }
     if (!l2) { l2 = new ListNode(0); }
-    t = l1.val + l2.val + f;
+    var t = l1.val + l2.val + f;
     cur = cur.next = new ListNode(t%10);
+    f = t > 9 ? 1 : 0;
+    l1 = l1.next;
+    l2 = l2.next;
   }
-  return ans;
+  return ans.next;
 }
