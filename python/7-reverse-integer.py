4763820346e688220b9d105ed15d6bef3c549d4f
dyl <dengyaolong@yeah.net>
2015-06-13 10:03:54 +0800

    7-reverse-integer python

--- python/7-reverse-integer.py
+++ python/7-reverse-integer.py
@@ -4,9 +4,3 @@ class Solution:
     else: a=1
     res = a * int(str(x)[::-1])
     return 0 if res >= 2**31 or res < -2**31 else res
-
-s = Solution()
-print s.reverse(-12345678909);
-print s.reverse(1234);
-print s.reverse(-1234);
-print s.reverse(1563847412), 2**31;
