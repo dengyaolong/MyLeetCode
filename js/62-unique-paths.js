f192dabb2dabdd1b1ddeb95bb64a35e8dec36313
dengyaolong <dengyaolong@yeah.net>
2017-09-11 21:43:21 +0800

    62

---
+++ js/62-unique-paths.js
@@ -0,0 +1,13 @@
+var uniquePaths = function(m, n) {
+    let a = {};
+    for(let i = 0; i < m; i++) {
+        a[i] = {}
+        for(let j = 0; j < n; j++) {
+            console.log(i, j)
+            if(j === 0) a[i][j] = 1
+            else if(i === 0) a[i][j] = 1
+            else a[i][j] = a[i][j - 1] + a[i - j][j]
+        }
+    }
+    return a[m-1][n-1]
+};
